96803534c4420d5bd908a94aa9a8e252cba18001
Jingzhenzxz <Jingzhen0000@outlook.com>
2024-06-17 22:14:57 +0800

    14.12 version

--- R3nzSkin/memory.hpp
+++ R3nzSkin/memory.hpp
@@ -65,7 +65,8 @@ private:
 		},
 		{
 			{
-				"48 8B 05 ? ? ? ? 48 8B 78 08 8B 40 10 4C 8D 3C C7 49 3B FF"
+				// "48 8B 05 ? ? ? ? 48 8B 78 08 8B 40 10 4C 8D 3C C7 49 3B FF"
+				"48 8B 1D ? ? ? ? E8 ? ? ? ? 48 8B 7B"
 			}, true, false, true, 0, &offsets::global::ManagerTemplate_AIHero_
 		},
 		{
